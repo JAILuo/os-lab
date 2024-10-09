c183ee2e210a223f1ea3fb4743a45ec80093b870
tracer-nju <tracer@nju.edu.cn>
2024-10-09 17:28:09 +0800

    Linux JAI 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:28:09 up 21:41,  1 user,  load average: 0.91, 0.71, 0.31

---
+++ .shadow/pstree/pstree.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <assert.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void read_proc(const char *proc_dir) {
+    char path[256] = {0};
+    int fd = 0;
+    ssize_t bytes_read = 0;
+    char buf[1024] = {0};
+
+    snprintf(path, sizeof(path), "/proc/%s/stat", proc_dir);
+
+    fd = open(path, O_RDONLY);
+    if (fd == -1) {
+        perror("Failed to open file");
+        return;
+    }
+    
+    bytes_read = read(fd, buf, sizeof(buf));
+    if (bytes_read == -1) {
+        perror("Failed to read file");
+    } else {
+        assert(bytes_read <= 1024);
+        buf[bytes_read] = '\0';
+        int pid, ppid;
+        char process_stat;
+        char name[256];
+        sscanf(buf, "%d (%255[^)]) %c %d", &pid, name, &process_stat, &ppid);
+        printf("pid: %d  name: %s  process_stat: %c  ppid: %d\n",
+               pid, name, process_stat, ppid);
+
+        //printf("Process path: %s:\nbuf: %s\n", path, buf);
+    }
+    close(fd);
+}
+
+void read_proc_dir() {
+    DIR *dir = NULL;  
+    struct dirent *entry = NULL;
+
+    dir = opendir("/proc");
+    if (dir == NULL) {
+        perror("Failed to open /proc directory");
+        return;
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_DIR) {
+            // use number to judge process, ok?
+            if (entry->d_name[0] >= '0' && entry->d_name[0] <= '9') {
+                //printf("PID: %s\n", entry->d_name);
+                read_proc(entry->d_name);
+            }
+        }
+    }
+    closedir(dir);
+}
+
+int main(int argc, char *argv[]) {
+    for (int i = 0; i < argc; i++) {
+        assert(argv[i]);
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
+    assert(!argv[argc]);
+
+    read_proc_dir();
+
+    return 0;
+}
+
