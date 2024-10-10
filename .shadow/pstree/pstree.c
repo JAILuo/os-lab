240313d4883d3ef25d2a7500475e837383d04130
tracer-nju <tracer@nju.edu.cn>
2024-10-10 17:21:20 +0800

    Linux JAI 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:21:20 up  1:57,  1 user,  load average: 0.24, 0.13, 0.10

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -14,6 +14,7 @@ typedef struct proc_node {
     int pid;
     int ppid;
     char name[256];
+    struct proc_node *parent;
     struct proc_node *child;   // 指向第一个子进程
     struct proc_node *next;    // 指向下一个兄弟进程
 } proc_node;
@@ -28,50 +29,11 @@ proc_node* create_proc_node(int pid, int ppid, const char *name) {
     node->ppid = ppid;
     assert(sizeof(node->name) <= 256);
     strncpy(node->name, name, sizeof(node->name));
+    node->parent = NULL;
     node->child = NULL;
     node->next = NULL;
 
     return node;
-}
-
-proc_node *find_proc_node(proc_node *root, int pid) {
-    if (root == NULL) return NULL;
-    if (root->pid == pid) return root;
-
-    proc_node *child = root->child;
-    while (child) {
-        proc_node *result = find_proc_node(child, pid);
-        if (result) return result;
-        
-        child = child->next;
-    }
-    return find_proc_node(root->next, pid);
-}
-
-void add_proc_node(proc_node **root, proc_node *node) {
-    if (node->ppid == (*root)->pid) {
-        // 新节点的父节点是根节点
-        node->next = (*root)->child;
-        (*root)->child = node;
-        printf("node->pid: %d, ppid: %d\n", node->pid, node->ppid);
-    } else {
-        // 遍历兄弟节点，寻找父节点
-        proc_node *current = *root;
-        while (current) {
-            if (current->pid == node->ppid) {
-                node->next = current->child;
-                current->child = node;
-                return;
-            }
-            current = current->next;
-        }
-        // 如果没有找到父节点，将新节点添加到树的末尾
-        current = *root;
-        while (current->next != NULL) {
-            current = current->next;
-        }
-        current->next = node;
-    }
 }
 
 void read_proc(const char *proc_dir) {
@@ -89,30 +51,19 @@ void read_proc(const char *proc_dir) {
     }
     
     bytes_read = read(fd, buf, sizeof(buf));
-    if (bytes_read == -1) {
-        perror("Failed to read file");
-    } else {
-        assert(bytes_read <= 1024);
-        buf[bytes_read] = '\0';
-        int pid, ppid;
-        char process_stat;
-        char name[256];
-        sscanf(buf, "%d (%255[^)]) %c %d", &pid, name, &process_stat, &ppid);
-        //printf("pid: %d  name: %s  process_stat: %c  ppid: %d\n",
-        //       pid, name, process_stat, ppid);
+    assert(bytes_read != -1);
 
-        proc_node *new_node = create_proc_node(pid, ppid, name);
+    assert(bytes_read <= 1024);
+    buf[bytes_read] = '\0';
+    int pid, ppid;
+    char process_stat;
+    char name[256];
+    sscanf(buf, "%d (%255[^)]) %c %d", &pid, name, &process_stat, &ppid);
+    printf("pid: %d  name: %s  process_stat: %c  ppid: %d\n",
+           pid, name, process_stat, ppid);
 
-        if (root_node == NULL) {
-            root_node = new_node;
-        } else {
-            add_proc_node(&root_node, new_node);
-        }
-        // printf("Process path: %s:\nbuf: %s\n", path, buf);
-    }
     close(fd);
 }
-
 
 void read_proc_dir() {
     DIR *dir = NULL;  
@@ -132,23 +83,6 @@ void read_proc_dir() {
     closedir(dir);
 }
 
-void free_proc_tree(proc_node *node) {
-    if (node == NULL) return;
-    free_proc_tree(node->child);
-    free_proc_tree(node->next);
-    free(node);
-}
-
-void print_proc_tree(proc_node *node, int level) {
-    if (node == NULL) return;
-    for (int i = 0; i < level; i++) {
-        printf("    "); // 缩进表示层级
-    }
-    printf("%s (PID: %d, PPID: %d)\n", node->name, node->pid, node->ppid);
-    print_proc_tree(node->child, level + 1);  // 递归打印子节点
-    print_proc_tree(node->next, level);       // 打印兄弟节点
-}
-
 int main(int argc, char *argv[]) {
     for (int i = 0; i < argc; i++) {
         assert(argv[i]);
@@ -157,8 +91,6 @@ int main(int argc, char *argv[]) {
     assert(!argv[argc]);
 
     read_proc_dir();
-    print_proc_tree(root_node, 0);
-    free_proc_tree(root_node);
 
     return 0;
 }
