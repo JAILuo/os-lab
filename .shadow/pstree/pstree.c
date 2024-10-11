212440cd1062a69ff784a1042d8e6e2b9fcbfeff
tracer-nju <tracer@nju.edu.cn>
2024-10-11 09:34:51 +0800

    Linux JAI 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:34:51 up  9:00,  1 user,  load average: 0.47, 0.39, 0.27

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -7,8 +7,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-#define IS_DIGIT(c) ((c) >= '0' && (c) <= '9')
 
 typedef struct proc_node {
     int pid;
@@ -22,44 +22,97 @@ typedef struct proc_node {
 
 proc_node *root_node = NULL;
 
-// proc_node* create_proc_node(int pid, int ppid, const char *name) {
-//     proc_node *node = malloc(sizeof(proc_node));
-//     assert(node != NULL);
-// 
-//     node->pid = pid;
-//     node->ppid = ppid;
-//     assert(sizeof(node->name) <= 256);
-//     strncpy(node->name, name, sizeof(node->name));
-//     node->parent = NULL;
-//     node->child = NULL;
-//     node->next = NULL;
-// 
-//     return node;
-// }
+proc_node* create_proc_node(int pid, int ppid, const char *name) {
+    proc_node *node = malloc(sizeof(proc_node));
+    assert(node != NULL);
+
+    node->pid = pid;
+    node->ppid = ppid;
+    assert(sizeof(node->name) <= 256);
+    strncpy(node->name, name, sizeof(node->name));
+    node->parent = NULL;
+    node->child = NULL;
+    node->next = NULL;
+
+    return node;
+}
+
+proc_node *find_node(pid_t pid, proc_node *cur) {
+    if (cur == NULL) cur = root_node;
+    // 1. End of recursion 
+    if (cur->pid == pid) return cur;
+
+    // 2. Recursion 
+    // 2.1 search all child proc of the current node.
+    proc_node *next_child = cur->child;
+    while (next_child) {
+        proc_node *result = find_node(pid, next_child);
+        if (result) return result;
+        next_child = next_child->child;
+    }
+
+    // 2.2 search all sibling proc of the current node.
+    proc_node *next_sibling = cur->next;
+    while (next_sibling) {
+        proc_node *result = find_node(pid, next_sibling);
+        if (result) return result;
+        next_sibling = next_sibling->next;
+    }
+
+    return NULL;
+}
+
+void add_proc_node(proc_node *proc) {
+    proc_node *self = find_node(proc->pid, NULL);
+    if (self) return;
+
+    // 1. check proc if has parent
+    //    if the proc's parent dead, proc should be orphan.
+    //    we don't consider this.
+    proc_node *parent = find_node(proc->ppid, NULL);
+    if (parent == NULL) {
+        printf("orphan\n");
+    }
+
+    proc->parent = parent;
+
+    // 2. then parent if proc has child
+    proc_node *child = parent->child;
+    if (child == NULL) {
+        // without child
+        parent->child = proc;
+    } else {
+        // Parent has child, so the proc should have sibling(next)
+        // Find the last child in the list and add the new process there.
+        proc_node *last_child = child;
+        while (last_child->next) {
+            last_child = last_child->next;
+        }
+        last_child->next = proc;
+    }
+}
+
 
 void read_proc(const char *proc_dir) {
     char path[256] = {0};
-    int fd = 0;
-    char buf[1024] = {0};
 
     snprintf(path, sizeof(path), "/proc/%s/stat", proc_dir);
-    printf("path: %s\n", path);
+    //printf("path: %s\n", path);
     
     FILE *fp = fopen(path, "r");
     assert(fp != NULL);
-
 
     int pid, ppid;
     char name[256] = {0};
     char process_state;
     fscanf(fp, "%d (%255[^)]) %c %d", &pid, name, &process_state, &ppid);
     
-
-    //sscanf(buf, "%d (%255[^)]) %c %d", &pid, name, &process_stat, &ppid);
     printf("pid: %d  name: %s  process_stat: %c  ppid: %d\n",
            pid, name, process_state, ppid);
 
-    close(fd);
+    //proc_node *node = create_proc_node(pid, ppid, name);
+    //add_proc_node(node);
+    fclose(fp);
 }
 
 void read_proc_dir() {
@@ -72,8 +125,9 @@ void read_proc_dir() {
         return;
     }
 
+    
     while ((entry = readdir(dir)) != NULL) {
-        if (entry->d_type == DT_DIR && IS_DIGIT(entry->d_name[0])) {
+        if (entry->d_type == DT_DIR && isdigit(*(entry->d_name))) {
             read_proc(entry->d_name);
         }
     }
