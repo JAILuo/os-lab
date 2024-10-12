c486cb2b4ea88289cbf1eefbbbf80f08c087c74a
tracer-nju <tracer@nju.edu.cn>
2024-10-12 21:03:48 +0800

    Linux JAI 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:03:48 up 22:47,  1 user,  load average: 0.13, 0.16, 0.18

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -77,8 +77,8 @@ void printParentProcesses(proc_node* proc) {
 void printProcess(proc_node* proc) {
     printf("%s%s%s",
            (proc == &root_node ? "" : (proc == proc->parent->child ? 
-                                      (proc->next ? " ┬" : "───") : 
-                                      (proc->next ? " ├" : " ├")
+                                      (proc->next ? "┬" : "───") : 
+                                      (proc->next ? "├" : " ├")
                                      )),
            proc->name,
            proc->child ? "" : "\n");
