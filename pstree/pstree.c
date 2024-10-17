f23fed2c3ec6087d6cfa07e59be653f6351815c1
JAILuo <1670756477@qq.com>
2024-10-17 22:01:36 +0800

    add -n, but still memory leak

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <dirent.h>
+#include <sys/select.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -40,7 +41,6 @@ static bool op_numeric = false;
 void parse_option(int argc, char *argv[]) {
     for (int i = 0; i < argc; i++) {
         assert(argv[i]);
-        //printf("argv[%d] = %s\n", i, argv[i]);
     }
     assert(!argv[argc]);
 
@@ -51,15 +51,15 @@ void parse_option(int argc, char *argv[]) {
         case 'n': op_numeric = true; break;
         case 'V': printf("own pstree implementation version\n"); break;
         default:
-                  printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
+                  printf("Usage: %s [OPTION...]\n"
+                         "    [-p, --show-pids] [-n, --numeric-sort:] [-V, --version]\n",
+                         argv[0]);
                   exit(0);
         }
     }
 }
 
-
 // learn from github, I have truoble in printing the whole tree
-// TODO
 void printParentProcesses(proc_node* proc) {
     if (proc->parent) printParentProcesses(proc->parent);
     printf("%s%*s",
@@ -68,17 +68,19 @@ void printParentProcesses(proc_node* proc) {
 }
 
 /**
- * traverse the process tree.(preorder traversal)
+ * Traverse the process tree.(preorder traversal)
  * if proc is root_node, print nothing,
- * else if proc is the first child, and if it has a sibling,     print -+-, else ---
- * else if proc is not the first child, and if it has a sibling, print |- , else 
+ * else if proc is the first child, and if it has a sibling,     print─┬─, else───
+ * else if proc is not the first child, and if it has a sibling, print ├─, else └─
+ * notice the space
  *
+ * The font is directly copied from the pstree displayed in the terminal.
  */
 void printProcess(proc_node* proc) {
     printf("%s%s%s",
            (proc == &root_node ? "" : (proc == proc->parent->child ? 
                                       (proc->next ? "─┬─" : "───") : 
-                                      (proc->next ? " ├─" : " ├─")
+                                      (proc->next ? " ├─" : " └─")
                                      )),
            proc->name,
            proc->child ? "" : "\n");
@@ -89,28 +91,6 @@ void printProcess(proc_node* proc) {
         if (proc->next->parent) printParentProcesses(proc->next->parent);
         printProcess(proc->next);
     }
-}
-
-proc_node* create_proc_node(int pid, int ppid, const char *name) {
-    proc_node *node = malloc(sizeof(proc_node));
-    assert(node != NULL);
-
-    node->pid = pid;
-    node->ppid = ppid;
-
-    assert(sizeof(node->name) <= 256);
-        strncpy(node->name, name, sizeof(node->name));
-    if (op_show_pids) {
-        char name[16] = {0};
-        sprintf(name, "(%d)", node->pid);
-        strcat(node->name, name); // should enough
-    }
-
-    node->parent = NULL;
-    node->child = NULL;
-    node->next = NULL;
-
-    return node;
 }
 
 proc_node *find_node(pid_t pid, proc_node *cur) {
@@ -139,7 +119,33 @@ proc_node *find_node(pid_t pid, proc_node *cur) {
     return NULL; // Not found
 }
 
+proc_node* create_proc_node(int pid, int ppid, const char *name) {
+    proc_node *node = malloc(sizeof(proc_node));
+    assert(node != NULL);
+
+    node->pid = pid;
+    node->ppid = ppid;
+    //pid_table[pid_index++] = pid;
+
+    assert(sizeof(node->name) <= 256);
+    strncpy(node->name, name, sizeof(node->name));
+    node->process_state = '\0'; // unused
+    if (op_show_pids) {
+        char add_pid[16] = {0};
+        sprintf(add_pid, "(%d)", node->pid);
+        strcat(node->name, add_pid); // should enough
+    }
+
+    node->parent = NULL;
+    node->child = NULL;
+    node->next = NULL;
+
+    //printf("[create] name: %s  pid: %d  ppid: %d\n", node->name, node->pid, node->ppid);
+    return node;
+}
+
 void add_proc_node(proc_node *proc) {
+    if (proc == NULL) return;
     // 0. remove duplication
     proc_node *self = find_node(proc->pid, NULL);
     if (self) return;
@@ -157,15 +163,52 @@ void add_proc_node(proc_node *proc) {
         } else {
             // Parent has child, so the proc should have sibling(next)
             // Find the last child in the list and add the new process there.
-            proc_node *last_child = child;
-            while (last_child->next) {
-                last_child = last_child->next;
+            if (op_numeric) {
+                if (proc->pid < child->pid) {
+                    proc->next = child;
+                    parent->child = proc;
+                } else {
+                    while (child->next && proc->pid > child->next->pid) child = child->next;
+                    proc->next = child->next;
+                    child->next = proc;
+                }
+            } else {
+                proc_node *last_child = child;
+                while (last_child->next) {
+                    last_child = last_child->next;
+                }
+                last_child->next = proc;
             }
-            last_child->next = proc;
         }
     }
 }
 
+/**
+ * the flow same as find_node, 
+ * remember not to free root_node
+ */
+void free_proc_tree(proc_node *node) {
+    if (node == NULL) return;
+    
+    if (node->child) {
+        free_proc_tree(node->child);
+        node->child = NULL;
+    }
+
+    if (node->next) {
+        free_proc_tree(node->next);
+        node->next = NULL;
+    }
+
+    if (node != &root_node) {
+        //printf("[free] name: %s  pid: %d  ppid: %d\n", node->name, node->pid, node->ppid);
+        free(node);
+        node = NULL;
+    } else {
+        node->child = NULL;
+        node->next = NULL;
+    }
+}
 
 proc_node *read_proc(const char *proc_dir, proc_node *parent) {
     char path[256] = {0};
@@ -180,9 +223,9 @@ proc_node *read_proc(const char *proc_dir, proc_node *parent) {
     FILE *fp = fopen(path, "r");
     assert(fp != NULL);
 
-    int pid, ppid;
+    int pid = 0, ppid = 0;
     char name[256] = {0};
-    char process_state;
+    char process_state = 'X';
     fscanf(fp, "%d (%255[^)]) %c %d", &pid, name, &process_state, &ppid);
 
     //printf("pid: %d  name: %s  process_stat: %c  ppid: %d\n",
@@ -231,12 +274,16 @@ void read_proc_dir() {
     closedir(dir);
 }
 
+
+//TODO redirect stderr
 int main(int argc, char *argv[]) {
     parse_option(argc, argv);
 
     read_proc_dir();
 
     printProcess(&root_node);
+
+    free_proc_tree(&root_node);
 
     return 0;
 }
