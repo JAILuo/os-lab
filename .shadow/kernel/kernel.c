c718dec7de02450361f86390b4fe20031e080804
tracer-nju <tracer@nju.edu.cn>
2024-10-26 20:13:57 +0800

    Linux JAI 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:13:57 up 1 day, 13:44,  1 user,  load average: 0.14, 0.47, 0.78

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -131,15 +131,15 @@ void draw_image(const unsigned char* src,
     printf("src_width * src_height * 4: %d\n", src_width * src_height * sizeof(uint32_t));
     for (int y = src_height - 1; y >= 0; y--) {
         for (int x = src_width - 1; x >= 0; x--) {
-            // int offset = y * src_width;
-            // uint8_t b = *(((uint8_t*)&src[offset]) + 3 * x);
-            // uint8_t g = *(((uint8_t*)&src[offset]) + 3 * x + 1);
-            // uint8_t r = *(((uint8_t*)&src[offset]) + 3 * x + 2);
+            int offset = y * src_width;
+            uint8_t b = *(((uint8_t*)&src[offset]) + 3 * x);
+            uint8_t g = *(((uint8_t*)&src[offset]) + 3 * x + 1);
+            uint8_t r = *(((uint8_t*)&src[offset]) + 3 * x + 2);
 
-            int offset = y * src_width + x;
-            unsigned char r = src[offset * 3];
-            unsigned char g = src[offset * 3 + 1];
-            unsigned char b = src[offset * 3 + 2];
+            // int offset = y * src_width + x;
+            // unsigned char r = src[offset * 3];
+            // unsigned char g = src[offset * 3 + 1];
+            // unsigned char b = src[offset * 3 + 2];
             src_pixels[offset + x] = (r << 16) | (g << 8) | b;
             //printf("src_pixels: %x\n", src_pixels[offset]);
         }
@@ -176,7 +176,7 @@ int main(const char *args) {
 
   //splash();
 
-  draw_image(test_jpg, 0, 0, 640, 480);
+  draw_image(test_jpg, 0, 0, 64, 48);
   //splash();
 
   puts("Press any key to see its key code...\n");
