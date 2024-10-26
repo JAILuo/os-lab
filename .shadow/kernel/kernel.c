2fa93b521d395db4dd31277997ca73f070603888
tracer-nju <tracer@nju.edu.cn>
2024-10-26 15:04:29 +0800

    Linux JAI 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:04:29 up 1 day, 11:49,  1 user,  load average: 0.24, 0.52, 0.42

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -155,6 +155,7 @@ void draw_image(const unsigned char* src,
         for (int x = 0; x < screen_w; x++) {
             uint32_t color = dst_pixels[y * screen_w + x];
             draw_tile(dst_x + x, dst_y + y, 1, 1, color);
+            printf("x * y = %d\n", x * y);
         }
     }
 
