23672f42567da98d8edc734cf9a096c84403900d
tracer-nju <tracer@nju.edu.cn>
2024-10-26 16:20:08 +0800

    Linux JAI 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:20:08 up 1 day, 12:59,  1 user,  load average: 1.29, 1.39, 1.09

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -103,7 +103,7 @@ void resize_image(const uint32_t* src_pixels, int src_width, int src_height,
 }
 
 void sleep() {
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < 100; i++) {
     printf("..............\n");
     }
 }
