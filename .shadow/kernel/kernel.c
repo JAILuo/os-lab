fd87926ad4001cb972e9a05c839bad7eed0b6632
tracer-nju <tracer@nju.edu.cn>
2024-10-26 15:01:18 +0800

    Linux JAI 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:01:18 up 1 day, 11:45,  1 user,  load average: 0.97, 0.63, 0.42

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -139,13 +139,11 @@ void draw_image(const unsigned char* src,
     for (int y = 0; y < src_height; y++) {
         for (int x = 0; x < src_width; x++) {
             int offset = y * src_width + x;
-            unsigned char r = src[offset * 3 + 2];
+            unsigned char r = src[offset * 3];
             unsigned char g = src[offset * 3 + 1];
-            unsigned char b = src[offset * 3];
-            // unsigned char r = src[offset * 3];
-            // unsigned char g = src[offset * 3 + 1];
-            // unsigned char b = src[offset * 3 + 2];
+            unsigned char b = src[offset * 3 + 2];
             src_pixels[offset] = (0xFF << 24) | (r << 16) | (g << 8) | b;
+            printf("src_pixels: %x\n", src_pixels[offset]);
         }
     }
 
