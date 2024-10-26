beb4eef51212c61ac05996d3b15037f1c1a29ebc
tracer-nju <tracer@nju.edu.cn>
2024-10-26 15:14:05 +0800

    Linux JAI 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:14:05 up 1 day, 11:58,  1 user,  load average: 0.41, 0.56, 0.50

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -79,8 +79,10 @@ extern unsigned int test_jpg_len;
 
 void resize_image(const uint32_t* src_pixels, int src_width, int src_height,
                   uint32_t* dst_pixels, int dst_width, int dst_height) {
-    float x_scale = (float)src_width / dst_width;
-    float y_scale = (float)src_height / dst_height;
+    // float x_scale = (float)src_width / dst_width;
+    // float y_scale = (float)src_height / dst_height;
+    float x_scale = (float)dst_width / src_width;
+    float y_scale = (float)dst_height / src_height;
 
     for (int y = 0; y < dst_height; y++) {
         for (int x = 0; x < dst_width; x++) {
@@ -124,7 +126,6 @@ void draw_image(const unsigned char* src,
             unsigned char g = src[offset * 3 + 1];
             unsigned char b = src[offset * 3 + 2];
             src_pixels[offset] = (0xFF << 24) | (r << 16) | (g << 8) | b;
-            //printf("src_pixels: %x\n", src_pixels[offset]);
         }
     }
 
