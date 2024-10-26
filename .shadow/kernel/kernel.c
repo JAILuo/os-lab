b3b312130ef3c3f98a17e3d67cc67eef826c79e4
tracer-nju <tracer@nju.edu.cn>
2024-10-26 14:57:07 +0800

    Linux JAI 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:57:07 up 1 day, 11:41,  1 user,  load average: 0.20, 0.22, 0.27

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -79,13 +79,10 @@ extern unsigned int test_jpg_len;
 
 void resize_image(const uint32_t* src_pixels, int src_width, int src_height,
                   uint32_t* dst_pixels, int dst_width, int dst_height) {
-    int screen_w, screen_h;
-    get_screen_size(&screen_w, &screen_h);
-    // float x_scale = (float)src_width / dst_width;
-    // float y_scale = (float)src_height / dst_height;
-    float x_scale = (float)screen_w / dst_width;
-    float y_scale = (float)screen_h / dst_height;
-    //printf("src_width: %d  src_height: %d\n", src_width, src_height);
+    float x_scale = (float)src_width / dst_width;
+    float y_scale = (float)src_height / dst_height;
+    printf("src_width: %d  src_height: %d\n", src_width, src_height);
+    printf("x_scale: %f  y_scale: %f\n", x_scale, y_scale);
 
     for (int y = 0; y < dst_height; y++) {
         for (int x = 0; x < dst_width; x++) {
