ec79a30a3e396eafcba4484c08eb804cc6a746f7
tracer-nju <tracer@nju.edu.cn>
2024-10-26 16:08:23 +0800

    Linux JAI 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:08:23 up 1 day, 12:47,  1 user,  load average: 2.16, 1.25, 0.78

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -160,7 +160,7 @@ int main(const char *args) {
 
   //splash();
 
-  draw_image(test_jpg, 0, 0, 2048, 2048);
+  draw_image(test_jpg, 0, 0, 320, 320);
 
   puts("Press any key to see its key code...\n");
   while (1) {
