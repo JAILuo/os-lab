846eaf39ab8df787fbc85d0945023d3a10eceec7
tracer-nju <tracer@nju.edu.cn>
2024-10-27 16:52:01 +0800

    Linux JAI 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:52:01 up 1 day, 20:58,  1 user,  load average: 1.45, 1.10, 0.60

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -133,12 +133,12 @@ void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, i
     }
 
     // 缩放图片
-    resize_image(src_pixels, src_width, src_height, dst_pixels, screen_w, screen_h);
+    //resize_image(src_pixels, src_width, src_height, dst_pixels, screen_w, screen_h);
 
     // 绘制图片
     for (int y = 0; y < screen_h; y++) {
         for (int x = 0; x < screen_w; x++) {
-            uint32_t color = dst_pixels[y * screen_w + x];
+            uint32_t color = src_pixels[y * screen_w + x];
             draw_tile(dst_x + x, dst_y + y, 16, 16, color);
             //printf("x * y = %d\n", x * y);
         }
