44e18c7b97d3ad46c4c393054cc469af326fae4b
tracer-nju <tracer@nju.edu.cn>
2024-10-27 16:03:37 +0800

    Linux JAI 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:03:37 up 1 day, 20:10,  1 user,  load average: 1.13, 1.14, 0.81

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -105,9 +105,9 @@ void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, i
     int screen_w, screen_h;
     get_screen_size(&screen_w, &screen_h);
 
-  for (int i = 0; i < test_jpg_len ; i++) {
-   printf("src[%d]: %x\n", i, src[i]);
-  }
+ // for (int i = 0; i < test_jpg_len ; i++) {
+ //  printf("src[%d]: %x\n", i, src[i]);
+ // }
 
     // 分配内存来存储缩放后的像素数据
     uint32_t* dst_pixels = (uint32_t*)malloc(screen_w * screen_h * sizeof(uint32_t));
@@ -147,9 +147,9 @@ void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, i
         }
     }
 
-    for (int i = 0; i < test_jpg_len; i++) {
-        printf("src[%d]: %x\n", i, src_pixels[i]);
-    }
+ //    for (int i = 0; i < test_jpg_len; i++) {
+ //        printf("src[%d]: %x\n", i, src_pixels[i]);
+ //    }
 
     // 缩放图片
     resize_image(src_pixels, src_width, src_height, dst_pixels, screen_w, screen_h);
