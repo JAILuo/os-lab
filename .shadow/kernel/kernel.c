aaaaa83589324954d1edc4a0f3cfc555fd8edb46
tracer-nju <tracer@nju.edu.cn>
2024-10-27 17:03:50 +0800

    Linux JAI 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:03:50 up 1 day, 21:10,  1 user,  load average: 1.39, 1.25, 0.87

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -100,7 +100,7 @@ void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, i
     get_screen_size(&screen_w, &screen_h);
 
     // 分配内存来存储缩放后的像素数据
-    uint32_t* dst_pixels = (uint32_t*)malloc(screen_w * screen_h * 4);
+    uint32_t* dst_pixels = (uint32_t*)malloc(screen_w * screen_h * 3);
     if (!dst_pixels) {
         printf("Memory allocation failed\n");
         return;
@@ -108,7 +108,7 @@ void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, i
     printf("screen_w * screen_h * 3: %d\n", screen_w * screen_h * 3);
 
     // 将图片数据转换为32位RGB格式
-    uint32_t* src_pixels = (uint32_t*)malloc(src_width * src_height * 4);
+    uint32_t* src_pixels = (uint32_t*)malloc(src_width * src_height * 3);
     if (!src_pixels) {
         printf("Memory allocation failed\n");
         free(dst_pixels);
@@ -127,7 +127,7 @@ void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, i
             // unsigned char r = src[offset * 3];
             // unsigned char g = src[offset * 3 + 1];
             // unsigned char b = src[offset * 3 + 2];
-            // src_pixels[offset] = (r << 16) | (g << 8) | b;
+            //src_pixels[offset] = (r << 16) | (g << 8) | b;
             //printf("src_pixels: %x\n", src_pixels[offset]);
         }
     }
