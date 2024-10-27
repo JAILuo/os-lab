de35afdffb305f9f0cfe9437ee34582dd85c823d
tracer-nju <tracer@nju.edu.cn>
2024-10-27 17:17:46 +0800

    Linux JAI 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:17:46 up 1 day, 21:24,  1 user,  load average: 0.85, 1.02, 0.98

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -123,15 +123,11 @@ void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, i
             // uint8_t r = *(((uint8_t*)&src[offset]) + 3 * x + 2);
             // src_pixels[offset + x] = (r << 16) | (g << 8) | b;
 
-            int offset = y * src_width;
-            uint8_t b = *(((uint8_t*)&src[offset]) + 3 * x + 2);
-            uint8_t g = *(((uint8_t*)&src[offset]) + 3 * x + 1);
-            uint8_t r = *(((uint8_t*)&src[offset]) + 3 * x);
-            // int offset = y * src_width + x;
-            // unsigned char r = src[offset * 3];
-            // unsigned char g = src[offset * 3 + 1];
-            // unsigned char b = src[offset * 3 + 2];
-            src_pixels[offset + x] = (r << 16) | (g << 8) | b;
+            int offset = y * src_width + x;
+            unsigned char r = src[offset * 3];
+            unsigned char g = src[offset * 3 + 1];
+            unsigned char b = src[offset * 3 + 2];
+            src_pixels[offset] = (r << 16) | (g << 8) | b;
             //printf("src_pixels: %x\n", src_pixels[offset]);
         }
     }
