f7fd0073fcc6468e3bdbf3f8e3cc2fbdc19f86d3
tracer-nju <tracer@nju.edu.cn>
2024-10-27 15:36:03 +0800

    Linux JAI 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:36:03 up 1 day, 19:42,  1 user,  load average: 0.48, 0.47, 0.46

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -113,15 +113,14 @@ void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, i
     printf("screen_w * screen_h * 4: %d\n", screen_w * screen_h * sizeof(uint32_t));
 
     // 将图片数据转换为32位ARGB格式
-    // uint32_t* src_pixels = (uint32_t*)malloc(src_width * src_height * sizeof(uint32_t));
-    // if (!src_pixels) {
-    //     printf("Memory allocation failed\n");
-    //     free(dst_pixels);
-    //     return;
-    // }
-    // printf("src_width * src_height * 4: %d\n", src_width * src_height * sizeof(uint32_t));
+    uint32_t* src_pixels = (uint32_t*)malloc(src_width * src_height * sizeof(uint32_t));
+    if (!src_pixels) {
+        printf("Memory allocation failed\n");
+        free(dst_pixels);
+        return;
+    }
+    printf("src_width * src_height * 4: %d\n", src_width * src_height * sizeof(uint32_t));
 
-    uint32_t src_pixels[src_width * src_height];
 
     for (int y = 0; y < src_height; y++) {
         for (int x = 0; x < src_width; x++) {
