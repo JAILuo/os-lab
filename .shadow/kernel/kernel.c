c55172ee4deb711b31439e89ad52a5c78496ffe8
tracer-nju <tracer@nju.edu.cn>
2024-10-27 16:13:09 +0800

    Linux JAI 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:13:09 up 1 day, 20:19,  1 user,  load average: 0.57, 0.49, 0.59

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -78,102 +78,14 @@ extern unsigned int test_jpg_len;
  * 3. 绘制图片
  */
 
-// void resize_image(uint32_t* src_pixels, int src_width, int src_height, uint32_t* dst_pixels, int dst_width, int dst_height) {
-//     for (int y = 0; y < dst_height; y++) {
-//         for (int x = 0; x < dst_width; x++) {
-//             // 计算目标像素在源图像中的位置
-//             int src_x = (int)(x * src_width / dst_width);
-//             int src_y = (int)(y * src_height / dst_height);
-// 
-//             // 确保坐标不越界
-//             src_x = src_x < src_width ? src_x : src_width - 1;
-//             src_y = src_y < src_height ? src_y : src_height - 1;
-// 
-//             // 获取源图像中最近邻像素的值
-//             uint32_t src_color = src_pixels[src_y * src_width + src_x];
-//             //printf("src_x: %d  src_y: %d\n", src_x, src_y);
-//             //printf("src_color: %x\n\n", src_color);
-// 
-//             // 将值赋给目标像素
-//             dst_pixels[y * dst_width + x] = src_color;
-//             //printf("src_color: %x dst_pixels: %x\n\n", src_color, dst_pixels[y * dst_width + x]);
-//         }
-//     }
-// }
-// 
-// void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, int src_height) {
-//     int screen_w, screen_h;
-//     get_screen_size(&screen_w, &screen_h);
-// 
-//  // for (int i = 0; i < test_jpg_len ; i++) {
-//  //  printf("src[%d]: %x\n", i, src[i]);
-//  // }
-// 
-//     // 分配内存来存储缩放后的像素数据
-//     uint32_t* dst_pixels = (uint32_t*)malloc(screen_w * screen_h * sizeof(uint32_t));
-//     if (!dst_pixels) {
-//         printf("Memory allocation failed\n");
-//         return;
-//     }
-//     printf("screen_w * screen_h * 4: %d\n", screen_w * screen_h * sizeof(uint32_t));
-// 
-//     // 将图片数据转换为32位ARGB格式
-//     uint32_t* src_pixels = (uint32_t*)malloc(src_width * src_height * sizeof(uint32_t));
-//     if (!src_pixels) {
-//         printf("Memory allocation failed\n");
-//         free(dst_pixels);
-//         return;
-//     }
-//     printf("src_width * src_height * 4: %d\n", src_width * src_height * sizeof(uint32_t));
-// 
-// 
-//     for (unsigned int y = 0; y < src_height; y++) {
-//         for (unsigned int x = 0; x < src_width; x++) {
-//             unsigned int offset = y * src_width;
-//             //printf("offset: %d\n", offset); 
-//             //printf("src[%d]: %d\n", offset + 3 * x, src[offset + 3 * x]);
-// 
-//             if (x * y > test_jpg_len) {
-//                 break;
-//             }
-// 
-//             uint8_t r = *(((uint8_t*)&src[offset]) + 3 * x + 2);
-//             uint8_t g = *(((uint8_t*)&src[offset]) + 3 * x + 1);
-//             uint8_t b = *(((uint8_t*)&src[offset]) + 3 * x);
-// 
-//             //printf("r: %d g: %d b: %d\n", r, g, b);
-//             src_pixels[offset + x] = (r << 16) | (g << 8) | b;
-//             //printf("src_pixels: 0x%x\n\n", src_pixels[offset + x]);
-//         }
-//     }
-// 
-//  //    for (int i = 0; i < test_jpg_len; i++) {
-//  //        printf("src[%d]: %x\n", i, src_pixels[i]);
-//  //    }
-// 
-//     // 缩放图片
-//     resize_image(src_pixels, src_width, src_height, dst_pixels, screen_w, screen_h);
-// 
-//     // 绘制图片
-//     for (int y = 0; y < screen_h; y++) {
-//         for (int x = 0; x < screen_w; x++) {
-//             uint32_t color = dst_pixels[y * screen_w + x];
-//             draw_tile(x * 32, y * 32, 32, 32, color);
-//         }
-//     }
-// 
-//     free(src_pixels);
-//     free(dst_pixels);
-// }
-
 void resize_image(const uint32_t* src_pixels, int src_width, int src_height,
                   uint32_t* dst_pixels, int dst_width, int dst_height) {
-    // float x_scale = (float)src_width / dst_width;
-    // float y_scale = (float)src_height / dst_height;
+    float x_scale = (float)src_width / dst_width;
+    float y_scale = (float)src_height / dst_height;
     // float x_scale = (float)dst_width / src_width;
     // float y_scale = (float)dst_height / src_height;
-    float x_scale = (float)dst_width / src_width;
-    float y_scale = (float)dst_height / src_height;
+    //float x_scale = (float)dst_width / src_width;
+    //float y_scale = (float)dst_height / src_height;
 
     for (int y = 0; y < dst_height; y++) {
         for (int x = 0; x < dst_width; x++) {
