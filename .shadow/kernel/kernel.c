d8323668d3069ee10f97100d4d424065c77b31bc
tracer-nju <tracer@nju.edu.cn>
2024-10-29 17:46:09 +0800

    Linux JAI 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:46:09 up 2 days,  2:43,  1 user,  load average: 2.05, 1.38, 0.91

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -108,57 +108,58 @@ void sleep() {
     }
 }
 
-// void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, int src_height) {
-//     int screen_w, screen_h;
-//     get_screen_size(&screen_w, &screen_h);
-// 
-//     uint32_t* dst_pixels = (uint32_t*)malloc(screen_w * screen_h * 4);
-//     if (!dst_pixels) {
-//         printf("Memory allocation failed\n");
-//         return;
-//     }
-// 
-//     uint32_t* src_pixels = (uint32_t*)malloc(src_width * src_height * 4);
-//     if (!src_pixels) {
-//         printf("Memory allocation failed\n");
-//         free(dst_pixels);
-//         return;
-//     }
-// 
-//     // 跳过BMP文件头
-//     src += 54;
-// 
-//     // 计算每行的填充字节
-//     int line_padding = (4 - (src_width * 3) % 4) % 4;
-// 
-//     // BMP应该是(B G R)
-//     for (int y = src_height - 1; y >= 0; y--) {
-//         for (int x = 0; x < src_width; x++) {
-//             int offset = y * src_width + x;
-// 
-//             unsigned char r = src[(offset * 3) + 2];
-//             unsigned char g = src[(offset * 3) + 1];
-//             unsigned char b = src[(offset * 3) + 0];
-//             src_pixels[offset] = (r << 16) | (g << 8) | b;
-//         }
-//         // 跳过每行的填充字节
-//         src += line_padding;
-//     }
-// 
-//     resize_image(src_pixels, src_width, src_height, dst_pixels, screen_w, screen_h);
-// 
-//     // 绘制图片
-//     for (int y = 0; y < screen_h; y++) {
-//         for (int x = 0; x < screen_w; x++) {
-//             uint32_t color = dst_pixels[y * screen_w + x];
-//             draw_tile(x + dst_x, y + dst_y, 1, 1, color);
-//         }
-//     }
-// 
-//     free(src_pixels);
-//     free(dst_pixels);
-// }
+void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, int src_height) {
+    int screen_w, screen_h;
+    get_screen_size(&screen_w, &screen_h);
 
+    uint32_t* dst_pixels = (uint32_t*)malloc(screen_w * screen_h * 4);
+    if (!dst_pixels) {
+        printf("Memory allocation failed\n");
+        return;
+    }
+
+    uint32_t* src_pixels = (uint32_t*)malloc(src_width * src_height * 4);
+    if (!src_pixels) {
+        printf("Memory allocation failed\n");
+        free(dst_pixels);
+        return;
+    }
+
+    // 跳过BMP文件头
+    src += 54;
+
+    // 计算每行的填充字节
+    int line_padding = (4 - (src_width * 3) % 4) % 4;
+
+    // BMP应该是(B G R)
+    for (int y = src_height - 1; y >= 0; y--) {
+        for (int x = 0; x < src_width; x++) {
+            int offset = y * src_width + x;
+
+            unsigned char r = src[(offset * 3) + 2];
+            unsigned char g = src[(offset * 3) + 1];
+            unsigned char b = src[(offset * 3) + 0];
+            src_pixels[offset] = (r << 16) | (g << 8) | b;
+        }
+        // 跳过每行的填充字节
+        src += line_padding;
+    }
+
+    resize_image(src_pixels, src_width, src_height, dst_pixels, screen_w, screen_h);
+
+    // 绘制图片
+    for (int y = 0; y < screen_h; y++) {
+        for (int x = 0; x < screen_w; x++) {
+            uint32_t color = dst_pixels[y * screen_w + x];
+            draw_tile(x + dst_x, y + dst_y, 1, 1, color);
+        }
+    }
+
+    free(src_pixels);
+    free(dst_pixels);
+}
+
+#ifdef NEW
 void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, int src_height) {
     int screen_w, screen_h;
     get_screen_size(&screen_w, &screen_h);
@@ -205,68 +206,72 @@ void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, i
     free(src_pixels);
     free(dst_pixels);
 }
+#endif
 
-// void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, int src_height) {
-//     int screen_w, screen_h;
-//     get_screen_size(&screen_w, &screen_h);
-// 
-//     // 640 * 480 * 4 = 1228800 ~= 1.17 MiB 
-//     uint32_t* dst_pixels = (uint32_t*)malloc(screen_w * screen_h* 4);
-//     if (!dst_pixels) {
-//         printf("Memory allocation failed\n");
-//         return;
-//     }
-//     //printf("screen_w * screen_h * 4: %d\n", screen_w * screen_h * 4);
-// 
-//     uint32_t* src_pixels = (uint32_t*)malloc(src_width * src_height * 4);
-//     if (!src_pixels) {
-//         printf("Memory allocation failed\n");
-//         free(dst_pixels);
-//         return;
-//     }
-//     //printf("src_width * src_height * 4: %d\n", src_width * src_height * 4);
-// 
-// 
-//     src = (uint8_t *)src + 54;
-//     // BMP shoulud be (B G R)
-//     int line_padding = (4 - (src_width * 3) % 4) % 4;
-//     for (int y = src_height - 1; y >= 0; y--) {
-//         //for (int x = 0; x < src_width / 3; x++) {
-//         for (int x = src_width; x >= 0; x--) {
-//             int offset = y * src_width + x;
-// 
-//             // uint8_t b = *(((uint8_t*)&src[src_width * y]) + 3 * x);
-//             // uint8_t g = *(((uint8_t*)&src[src_width * y]) + 3 * x + 1);
-//             // uint8_t r = *(((uint8_t*)&src[src_width * y]) + 3 * x + 2);
-//             // src_pixels[offset] = (r << 16) | (g << 8) | b;
-// 
-//             //little-endian, (low addr) B-G-R (high addr)
-//             unsigned char r = src[offset * 3 + 2];
-//             unsigned char g = src[offset * 3 + 1];
-//             unsigned char b = src[offset * 3];
-//             src_pixels[offset] = (r << 16) | (g << 8) | b;
-// 
-//             // printf("offset: %d\n", offset);
-//             // printf("b: %x  g: %x  r: %x\n", b, g, r);
-//             // printf("src_pixels: %x\n\n", src_pixels[offset]);
-//         }
-//         src += line_padding;
-//     }
-// 
-//     // 缩放图片
-//     resize_image(src_pixels, src_width, src_height, dst_pixels, screen_w, screen_h);
-// 
-//     // 绘制图片
-//     for (int y = screen_h; y >= 0; y--) {
-//         for (int x = 0; x < screen_w; x++) {
-//             uint32_t color = dst_pixels[y * screen_w + x];
-//             draw_tile(x + dst_x, y + dst_y, 1, 1, color);
-//         }
-//     }
-// 
-//     free(src_pixels);
-//     free(dst_pixels);
-// }
+
+#ifdef OWN
+void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, int src_height) {
+    int screen_w, screen_h;
+    get_screen_size(&screen_w, &screen_h);
+
+    // 640 * 480 * 4 = 1228800 ~= 1.17 MiB 
+    uint32_t* dst_pixels = (uint32_t*)malloc(screen_w * screen_h* 4);
+    if (!dst_pixels) {
+        printf("Memory allocation failed\n");
+        return;
+    }
+    //printf("screen_w * screen_h * 4: %d\n", screen_w * screen_h * 4);
+
+    uint32_t* src_pixels = (uint32_t*)malloc(src_width * src_height * 4);
+    if (!src_pixels) {
+        printf("Memory allocation failed\n");
+        free(dst_pixels);
+        return;
+    }
+    //printf("src_width * src_height * 4: %d\n", src_width * src_height * 4);
+
+
+    src = (uint8_t *)src + 54;
+    // BMP shoulud be (B G R)
+    int line_padding = (4 - (src_width * 3) % 4) % 4;
+    for (int y = src_height - 1; y >= 0; y--) {
+        //for (int x = 0; x < src_width / 3; x++) {
+        for (int x = src_width; x >= 0; x--) {
+            int offset = y * src_width + x;
+
+            // uint8_t b = *(((uint8_t*)&src[src_width * y]) + 3 * x);
+            // uint8_t g = *(((uint8_t*)&src[src_width * y]) + 3 * x + 1);
+            // uint8_t r = *(((uint8_t*)&src[src_width * y]) + 3 * x + 2);
+            // src_pixels[offset] = (r << 16) | (g << 8) | b;
+
+            //little-endian, (low addr) B-G-R (high addr)
+            unsigned char r = src[offset * 3 + 2];
+            unsigned char g = src[offset * 3 + 1];
+            unsigned char b = src[offset * 3];
+            src_pixels[offset] = (r << 16) | (g << 8) | b;
+
+            // printf("offset: %d\n", offset);
+            // printf("b: %x  g: %x  r: %x\n", b, g, r);
+            // printf("src_pixels: %x\n\n", src_pixels[offset]);
+        }
+        src += line_padding;
+    }
+
+    // 缩放图片
+    resize_image(src_pixels, src_width, src_height, dst_pixels, screen_w, screen_h);
+
+    // 绘制图片
+    for (int y = screen_h; y >= 0; y--) {
+        for (int x = 0; x < screen_w; x++) {
+            uint32_t color = dst_pixels[y * screen_w + x];
+            draw_tile(x + dst_x, y + dst_y, 1, 1, color);
+        }
+    }
+
+    free(src_pixels);
+    free(dst_pixels);
+}
+#endif
 
 // Operating system is a C program!
 int main(const char *args) {
