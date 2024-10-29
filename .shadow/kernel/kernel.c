8ffda84020f1eb8e3c8d3d91e629303aee35cd42
tracer-nju <tracer@nju.edu.cn>
2024-10-29 22:13:58 +0800

    Linux JAI 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:13:58 up  1:41,  1 user,  load average: 1.09, 0.54, 0.64

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -202,7 +202,7 @@ void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, i
     // BMP shoulud be (B G R)
     for (int y = src_height - 1; y >= 0; y--) {
         for (int x = 0; x < src_width; x++) {
-            int src_index = (y * (src_width * 3 + line_padding)) + (x * 3);
+            int src_index = (y * (src_width * 4 + line_padding)) + (x * 4);
             // int src_index = (y * 3 * src_width) + (x * 4);
             // little-endian, (low addr) B-G-R (high addr)
             unsigned char b = src[src_index + 0];
