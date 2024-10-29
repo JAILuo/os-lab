27ece9e78f7498a314d5629c22ff083bdd5f5818
tracer-nju <tracer@nju.edu.cn>
2024-10-29 22:09:19 +0800

    Linux JAI 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:09:19 up  1:37,  1 user,  load average: 0.90, 0.58, 0.72

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -196,8 +196,8 @@ void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, i
 
     // 每行的填充字节
     //int line_padding = ((src_width * 3 + 31) & ~31) - (src_width * 3);
-    //int line_padding = (4 - (src_width * 3) % 4) % 4;
-    int line_padding = ((src_width * 3 + 31) & ~31);
+    int line_padding = (4 - (src_width * 3) % 4) % 4;
+    //int line_padding = ((src_width * 3 + 31) & ~31);
 
     // BMP shoulud be (B G R)
     for (int y = src_height - 1; y >= 0; y--) {
