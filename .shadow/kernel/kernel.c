d0f93a4772a8ab8a3ee3ca591eb565ae215d16e1
tracer-nju <tracer@nju.edu.cn>
2024-10-29 21:48:12 +0800

    Linux JAI 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:48:12 up  1:16,  1 user,  load average: 1.79, 1.35, 1.03

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -249,6 +249,7 @@ void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, i
     for (int y = src_height - 1; y >= 0; y--) {
         for (int x = 0; x < src_width; x++) {
             int src_index = (y * (src_width * 4 + line_padding)) + (x * 4);
+            printf("line_padding: %d\n", line_padding);
             //int src_index = (y * 3 * src_width) + (x * 4);
             unsigned char b = src[src_index + 0];
             unsigned char g = src[src_index + 1];
