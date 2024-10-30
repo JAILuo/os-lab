574c031619fef07abd44667e64bdc1e3000e8939
tracer-nju <tracer@nju.edu.cn>
2024-10-30 10:56:24 +0800

    Linux JAI 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:56:24 up  6:15,  1 user,  load average: 2.38, 1.64, 0.93

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -124,8 +124,8 @@ void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, i
     src = (uint8_t *)src + 54; // jump BMP file header 
 
     // 每行的填充字节
-    //int line_padding = ((src_width * 3 + 31) & ~31) - (src_width * 3);
-    int line_padding = (4 - (src_width * 3) % 4) % 4;
+    int line_padding = ((src_width * 3 + 31) & ~31) - (src_width * 4);
+    //int line_padding = (4 - (src_width * 3) % 4) % 4;
     //int line_padding = ((src_width * 3 + 31) & ~31);
 
     // BMP shoulud be (B G R)
@@ -133,6 +133,7 @@ void draw_image(const unsigned char* src, int dst_x, int dst_y, int src_width, i
     // The bmp image starts at the last line and is scanned upwards progressively
     for (int y = src_height - 1; y >= 0; y--) {
         for (int x = src_width - 1; x >= 0; x--) {
+            // 1 pixels <-> 4 pixels
             int src_index = (y * (src_width * 4 + line_padding)) + (x * 4);
             // int src_index = (y * 3 * src_width) + (x * 4);
             
