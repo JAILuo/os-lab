1254b91106dced170220c75ab985954238f68d3a
tracer-nju <tracer@nju.edu.cn>
2024-12-05 10:57:42 +0800

    Linux JAI 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:57:42 up 23:59,  1 user,  load average: 0.98, 0.83, 0.55

--- .shadow/libco/tests/main.c
+++ .shadow/libco/tests/main.c
@@ -30,6 +30,7 @@ static void work(void *arg) {
 
 static void test_1() {
 
+    printf("test111\n");
     struct co *thd1 = co_start("thread-1", work, "X");
     struct co *thd2 = co_start("thread-2", work, "Y");
 
