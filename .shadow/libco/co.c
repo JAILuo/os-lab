67c35f3eeccc3aa6b23412e29edbc136cb3dea06
tracer-nju <tracer@nju.edu.cn>
2024-12-08 19:30:50 +0800

    Linux JAI 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:30:50 up 2 days,  4:06,  1 user,  load average: 0.08, 0.12, 0.18

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -103,8 +103,8 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
     struct co* new_co= (struct co*)malloc(sizeof(struct co));
     new_co->status = CO_NEW;
     strcpy(new_co->name, name);
-    //new_co->func = (void (*)(void *))func;
-    new_co->func = wrapper_;
+    new_co->func = (void (*)(void *))func;
+    //new_co->func = wrapper_;
     new_co->arg = co_list[co_num];
     new_co->waiter = NULL;
     memset(new_co->stack, 0, STACK_SIZE);
